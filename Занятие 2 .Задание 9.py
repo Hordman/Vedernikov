743e16a6d483c6c72c65c6c6973db18fa64c6a45
Hordman <90783455+Hordman@users.noreply.github.com>
2021-10-04 18:41:09 +0300

    Delete Занятие 2 .Задание 9.py

--- Занятие 2 .Задание 9.py
+++
@@ -1,7 +0,0 @@
-"# -- coding: utf-8 --"
-def D(n,m,k):
-    if (k<n*m) and (k%n==0 or k%m==0):
-        return "Да"
-    else:
-        return "Нет"
-print(D(abs(int(input("n:"))),abs(int(input("m:"))),abs(int(input("k:"))))) 
