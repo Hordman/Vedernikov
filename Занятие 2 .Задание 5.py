cf4e8ac0e3fa358f99bf19dd3c1ba61266ec3441
Hordman <90783455+Hordman@users.noreply.github.com>
2021-10-04 18:40:31 +0300

    Delete Занятие 2 .Задание 5.py

--- Занятие 2 .Задание 5.py
+++
@@ -1,9 +0,0 @@
-"# -- coding: utf-8 --"
-def D(a,b,c):
-    if a<=b and a<=c:
-        return a
-    elif b<= a and b<=c:
-        return b
-    else:
-        return c
-print(D(int(input("a:")),int(input("b:")),int(input("c:"))))   
